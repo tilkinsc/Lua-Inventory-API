22e5205b482197462311d167005344845d4709d5
Cody <deocojo1@gmail.com>
2016-03-27 12:37:29 -0400

    Unhid a item cloning function, put it in the object itself

--- Static Calls/InventoryUtil.lua
+++ Static Calls/InventoryUtil.lua
@@ -1,5 +1,7 @@
 
-local CloneItem = function(item)
+local InventoryUtil = {}
+
+InventoryUtil.CloneItem = function(item)
 	local out = {}
 	for i, v in pairs(item)do
 		if(type(v) == "table")then
@@ -13,8 +15,6 @@ local CloneItem = function(item)
 	return out
 end
 
-local InventoryUtil = {}
-
 -- TODO: Figure out how to handle return nil
 InventoryUtil.Add1D = function(inventory, inp)
 	if(inventory.Size < inventory.Length+1)then
@@ -26,7 +26,7 @@ InventoryUtil.Add1D = function(inventory, inp)
 end
 
 InventoryUtil.Add2D = function(inventory, x, y, inp)
-	if(x => inventory.LengthX+1)then
+	if(x >= inventory.LengthX+1)then
 		return nil
 	end
 	inventory.Contents[x][y] = inp
@@ -49,7 +49,7 @@ InventoryUtil.Fill1D = function(inventory, contents)
 	end
 	inventory.Contents = {}
 	for x=1, inventory.LengthX do
-		inventory.Contents[x] = CloneItem(contents)
+		inventory.Contents[x] = InventoryUtil.CloneItem(contents)
 	end
 	inventory.Size = #inventory.Contents
 end
@@ -63,7 +63,7 @@ InventoryUtil.Fill2D = function(inventory, contents)
 	for x=1, inventory.LengthX do
 		inventory.Contents[x] = {}
 		for y=1, inventory.LengthY do
-			inventory.Contents[x][y] = CloneItem(contents)
+			inventory.Contents[x][y] = InventoryUtil.CloneItem(contents)
 		end
 	end
 	inventory.LengthX = #inventory.Contents
